a05630999bd98d2aa9882f3b2f6f4dc6a5cd5eb3
Magneto-101 <mohitcodes99@gmail.com>
2024-02-02 11:39:34 +0530

    Area of a square

---
+++ chapter1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float side;
+    printf("Enter the side: \n");
+    scanf("%f", &side);
+
+    float area = side * side;
+    printf("The area is : %f \n", area);
+    return 0;
+
+
+}
